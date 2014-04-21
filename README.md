24fa75f898de581d69f5429f8321fc2ca8b51d6b
Ali Tayarani <ali.tayarani@tapjoy.com>
2014-04-21 13:52:36 -0400

    Initial version

--- README.md
+++ README.md
@@ -1,4 +1,76 @@
-tem
-===
+# AWS Account Manager
 
-TapJoy Environment Manager
+The AWS Account Manager is a shell function that manages multiple AWS accounts and their credentials. It handles setting
+all of the different variables required by the AWS command line tools (which are inconsistent). It also provides a
+mechanism for running commands under a different account without switching your local shell's context.
+
+## Installation
+
+Installation of the script is very simple:
+
+```
+$ curl -L https://raw.github.com/jlogsdon/aam/master/aam.sh > ~/.aam.sh
+$ echo 'source ~/.aam.sh' >> ~/.bash_profile # Or .zsh_profile if you use zshell
+$ exec $SHELL
+```
+
+You should now be able to call `aam`.
+
+## Usage
+
+```
+AWS Account Manager
+
+Usage:
+  aam help                      Show this message
+  aam create <account>          Create a new AWS account
+  aam use <account>             Use the named AWS account
+  aam do <account> <command...> Use the named AWS account
+  aam default <account>         Set the default account
+  aam list                      Show all available accounts
+```
+
+### Creating an Account
+
+Once AAM has been installed you can start by creating and editing new account:
+
+```
+$ aam create personal
+$ $EDITOR $AAM_STORE/personal
+```
+
+Account files are simply a list of variables which are sourced whenver you switch accounts. Anything goes as long as
+your shell can process the file. At a bare minimum the file should look like this:
+
+```
+export AWS_ACCESS_KEY=some_access_key_id
+export AWS_SECRET_KEY=some_secret_access_key
+```
+
+The next section describes all of the variables AAM works with, any of which can be defined explicitly in your account
+definition.
+
+## Variables
+
+Amazon's official command line tools have a huge problem with consistency. Different variables are read and, in some
+cases, a credentials file is required. The main goal for AAM, outside of making account switching simple, is to handle
+the setup of these variables and files automatically.
+
+* `AWS_ACCESS_KEY` and `AWS_SECRET_KEY` are expected by AAM and several of the command line tools.
+* `AWS_ACCESS_KEY_ID` is set to `AWS_ACCESS_KEY`.
+* `AWS_SECRET_ACCESS_KEY` is set to `AWS_SECRET_KEY`.
+* `AWS_CREDENTIALS_FILE` is, by default, set to `AAM_DEFAULT_EC2_CREDS`. You may override it in your account if you
+  wish.
+* `EC2_PRIVATE_KEY` should be set to the path of your private key. This is not handled magically and should be set in
+  either your account or globally in your profile.
+* `EC2_CERT` is similar to the above but should point to your certificate file.
+
+## Customization
+
+AAM uses it's own variables to define default accounts and where files should be stored. These can be overridden, but
+sane defaults are provided.
+
+* `AAM_STORE` is the folder where account definitions are stored (`$HOME/.aam`).
+* `AAM_DEFAULT_FILE` is the file where the default account is stored (`$AAM_STORE/.default`).
+* `AAM_DEFAULT_EC2_CREDS` is the default for `AWS_CREDENTIALS_FILE` (`$HOME/.ec2.creds`).
+
---
+++ em.sh
@@ -0,0 +1,593 @@
+# EM is a utility for managing multiple environment profiles. It handles loading a default set of variables, ensures
+# the litany of variables used by the tools are set and provides a mechanism for quickly switching between profiles.
+
+# Common ENV Stuff
+[ -z $EM_SCRIPT ] && export EM_SCRIPT=$0
+[ -z $EM_STORE ] && export EM_STORE=$HOME/.em
+
+# AWS ENV Stuff
+[ -z $AAM_STORE ] && export AAM_STORE=$EM_STORE/aws
+[ -d $AAM_STORE ] || mkdir -p $AAM_STORE
+[ -z $AAM_DEFAULT_FILE ] && export AAM_DEFAULT_FILE=${AAM_STORE}/.default
+
+[ -z $EC2_HOME ]              && export EC2_HOME="/usr/share/ec2-api-tools"
+[ -z $AWS_AUTO_SCALING_HOME ] && export AWS_AUTO_SCALING_HOME="/usr/share/as-api-tools"
+[ -z $AWS_CLOUDWATCH_HOME ]   && export AWS_CLOUDWATCH_HOME="/usr/share/cloudwatch-api-tools"
+[ -z $AAM_DEFAULT_EC2_CREDS ] && export AAM_DEFAULT_EC2_CREDS="$HOME/.ec2.creds"
+
+if [ -z $AAM_DEFAULT ]; then
+  if [ -f $AAM_DEFAULT_FILE ]; then
+    export AAM_DEFAULT=`cat ${AAM_DEFAULT_FILE}`
+  fi
+fi
+
+# Chef ENV Stuff
+[ -z $CEM_STORE ] && export CEM_STORE=$EM_STORE/chef
+[ -d $CEM_STORE ] || mkdir -p $CEM_STORE
+[ -z $CEM_DEFAULT_FILE ] && export CEM_DEFAULT_FILE=${CEM_STORE}/.default
+
+if [ -z $CEM_DEFAULT ]; then
+  if [ -f $CEM_DEFAULT_FILE ]; then
+    export CEM_DEFAULT=`cat ${CEM_DEFAULT_FILE}`
+  fi
+fi
+
+# General ENV stuff
+[ -z $TEM_STORE ] && export TEM_STORE=$EM_STORE/env
+[ -d $TEM_STORE ] || mkdir -p $TEM_STORE
+[ -z $TEM_DEFAULT_FILE ] && export TEM_DEFAULT_FILE=${TEM_STORE}/.default
+
+if [ -z $TEM_DEFAULT ]; then
+  if [ -f $TEM_DEFAULT_FILE ]; then
+    export TEM_DEFAULT=`cat ${TEM_DEFAULT_FILE}`
+  fi
+fi
+
+em() {
+  case $1 in
+    'aws')
+      shift
+      aam $@
+      ;;
+    'chef')
+      shift
+      cem $@
+      ;;
+    'env')
+      shift
+      tem $@
+      ;;
+  esac
+}
+##############################################################################
+# AAM is a utility for managing multiple AWS account credentials. It handles #
+# loading a default set of credentials, ensures the litany of variables used #
+# by the tools are set and provides a mechanism for quickly switching        #
+# between accounts.                                                          #
+##############################################################################
+
+aam() {
+  APPLICATION_NAME='AWS Account Manager'
+  COMMAND='aam'
+  if [ ! -z $1 ]; then
+    cmd=$1
+    shift
+  else
+    cmd='help'
+  fi
+  case $cmd in
+    "help" )
+      echo "${APPLICATION_NAME}"
+      echo
+      echo "Usage:"
+      echo "  ${COMMAND} help                      Show this message"
+      echo "  ${COMMAND} create <account>          Create a new AWS account"
+      echo "  ${COMMAND} remove <account>          Remove AWS account"
+      echo "  ${COMMAND} use <account>             Use the named AWS account"
+      echo "  ${COMMAND} do <account> <command...> Run a command under the given account"
+      echo "  ${COMMAND} default <account>         Set the default account"
+      echo "  ${COMMAND} list                      Show all available accounts"
+      echo
+      ;;
+    "create" )
+      local account
+      local store
+
+      if [ $# -lt 1 ]; then
+        echo "Usage: ${COMMAND} create <account>"
+        return 1
+      fi
+
+      account=$1
+      store=${AAM_STORE}/${account}
+      shift
+
+      if [ -f $store ]; then
+        echo "AWS account named ${account} already exists!"
+        return 1
+      fi
+
+      cat > $store <<EOC
+export AWS_ACCESS_KEY=
+export AWS_SECRET_KEY=
+EOC
+
+      echo "AWS account ${account} created! Edit ${store}, switch with ${COMMAND} use ${account}"
+      ;;
+    "remove")
+      local account
+      local store
+
+      if [ $# -lt 1 ]; then
+        echo "Usage: ${COMMAND} remove <account>"
+        return 1
+      fi
+
+      account=$1
+      store=${AAM_STORE}/${account}
+      shift
+
+      if [ -f $store ]; then
+        rm $store
+        return 0
+      else
+        echo "Chef profile ${account} does not exist at ${store}"
+        return 1
+      fi
+      ;;
+    "use" )
+      local account
+      local store
+
+      if [ $# -lt 1 ]; then
+        echo "Usage: ${COMMAND} use <account>"
+        return 1
+      fi
+
+      account=$1
+      shift
+
+      if [ $account = 'default' ]; then
+        if [ -z $AAM_DEFAULT ]; then
+          echo "No default account has been configured. Set one with ${COMMAND} default <account>."
+          return 1
+        fi
+        account=$AAM_DEFAULT
+      fi
+
+      store=${AAM_STORE}/${account}
+      if [ ! -f $store ]; then
+        echo "No account named ${account}"
+        return 1
+      fi
+
+      export AAM_ACCOUNT=$account
+
+      # Unset variables that may be set by the config
+      [ -z $AWS_CREDENTIAL_FILE ]   || unset AWS_CREDENTIAL_FILE
+
+      source $store
+
+      [ -z $AWS_CREDENTIAL_FILE ] && export AWS_CREDENTIAL_FILE=$AAM_DEFAULT_EC2_CREDS
+      export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY
+      export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_KEY
+
+      cat > $AWS_CREDENTIAL_FILE <<EOC
+AWSAccessKeyId=$AWS_ACCESS_KEY
+AWSSecretKey=$AWS_SECRET_KEY
+EOC
+
+      echo "Switched to account ${account}"
+      ;;
+    "do" )
+      local account
+
+      if [ $# -lt 1 ]; then
+        echo "Usage: aam do <account> <command...>"
+        return 1
+      fi
+
+      account=$1
+      store=${AAM_STORE}/${account}
+      shift
+      command=$*
+
+      if [ ! -f $store ]; then
+        echo "No account named ${account}"
+        return 1
+      fi
+
+      $SHELL -l -c "source ${EM_SCRIPT}; aam use ${account}; ${command}"
+      ;;
+    "default" )
+      local account
+
+      if [ $# -lt 1 ]; then
+        echo "Usage: aam default <account>"
+        return 1
+      fi
+
+      account=$1
+      store=${AAM_STORE}/${account}
+      shift
+
+      if [ ! -f $store ]; then
+        echo "No account named ${account}"
+        return 1
+      fi
+
+      echo $account > $AAM_DEFAULT_FILE
+      echo "Default account set to ${account}"
+      ;;
+    "list" )
+      echo
+      echo "Available accounts"
+      for account in `ls $AAM_STORE`; do
+        local ind=''
+        if [ "$account" = "$AAM_ACCOUNT" ]; then
+          ind='='
+          [ "$account" = "$AAM_DEFAULT" ] && ind+='*' || ind+='>'
+        else
+          ind=' '
+          [ "$account" = "$AAM_DEFAULT" ] && ind+='*' || ind+=' '
+        fi
+
+        echo "${ind} ${account}"
+      done
+      echo
+      echo '# => - current'
+      echo '# =* - current & default'
+      echo '#  * - default'
+      ;;
+  esac
+}
+
+############################################################################
+# CEM is a utility for managing multiple chef environments. It handles     #
+# loading a default set of variables, ensures the litany of variables      #
+# used by the tools are set and provides a mechanism for quickly switching #
+# between profiles.                                                        #
+############################################################################
+cem() {
+  APPLICATION_NAME='Chef Environment Manager'
+  COMMAND='cem'
+  if [ ! -z $1 ]; then
+    cmd=$1
+    shift
+  else
+    cmd='help'
+  fi
+  case $cmd in
+    "help" )
+      echo "${APPLICATION_NAME}"
+      echo
+      echo "Usage:"
+      echo "  ${COMMAND} help                      Show this message"
+      echo "  ${COMMAND} create <account>          Create a new AWS account"
+      echo "  ${COMMAND} remove <account>          Remove AWS account"
+      echo "  ${COMMAND} use <account>             Use the named AWS account"
+      echo "  ${COMMAND} do <account> <command...> Run a command under the given account"
+      echo "  ${COMMAND} default <account>         Set the default account"
+      echo "  ${COMMAND} list                      Show all available accounts"
+      ;;
+    "create" )
+      local account
+      local store
+
+      if [ $# -lt 1 ]; then
+        echo "Usage: ${COMMAND} create <account>"
+        return 1
+      fi
+
+      account=$1
+      store=${CEM_STORE}/${account}
+      shift
+
+      if [ -f $store ]; then
+        echo "Chef profile named ${account} already exists!"
+        return 1
+      fi
+
+       cat > $store <<EOC
+keypair_name = 
+hostname = 
+protocol = 'https'
+port = 443
+EOC
+
+      echo "Chef profile ${account} created! Edit ${store}, switch with ${COMMAND} use ${account}"
+      ;;
+    "remove")
+      local account
+      local store
+
+      if [ $# -lt 1 ]; then
+        echo "Usage: ${COMMAND} remove <account>"
+        return 1
+      fi
+
+      account=$1
+      store=${CEM_STORE}/${account}
+      shift
+
+      if [ -f $store ]; then
+        rm $store
+        return 0
+      else
+        echo "Chef profile ${account} does not exist at ${store}"
+        return 1
+      fi
+      ;;
+    "use" )
+      local account
+      local store
+
+      if [ $# -lt 1 ]; then
+        echo "Usage: ${COMMAND} use <account>"
+        return 1
+      fi
+
+      account=$1
+      shift
+
+      if [ $account = 'default' ]; then
+        if [ -z $CEM_DEFAULT ]; then
+          echo "No default account has been configured. Set one with aam default <account>."
+          return 1
+        fi
+        account=$CEM_DEFAULT
+      fi
+
+      store=${CEM_STORE}/${account}
+      if [ ! -f $store ]; then
+        echo "No account named ${account}"
+        return 1
+      fi
+
+      export CEM_ACCOUNT=$account
+      export CHEF_ENV_OVERRIDE='true'
+
+      rm -f $HOME/.chef/overrides/*
+      ln -sin $store $HOME/.chef/overrides/knife-${account}.rb
+
+      echo "Switched to account ${account}"
+      ;;
+     "do" )
+       local account
+
+       if [ $# -lt 1 ]; then
+         echo "Usage: ${COMMAND} do <account> <command...>"
+         return 1
+       fi
+
+       account=$1
+       store=${CEM_STORE}/${account}
+       shift
+       command=$*
+
+       if [ ! -f $store ]; then
+         echo "No account named ${account}"
+         return 1
+       fi
+
+       $SHELL -l -c "source ${EM_SCRIPT}; ${COMMAND} use ${account}; ${command}"
+       ;;
+    "default" )
+      local account
+
+      if [ $# -lt 1 ]; then
+        echo "Usage: ${COMMAND} default <account>"
+        return 1
+      fi
+
+      account=$1
+      store=${CEM_STORE}/${account}
+      shift
+
+      if [ ! -f $store ]; then
+        echo "No account named ${account}"
+        return 1
+      fi
+
+      echo $account > $CEM_DEFAULT_FILE
+      echo "Default account set to ${account}"
+      ;;
+    "list" )
+      echo
+      echo "Available accounts"
+      for account in $(ls $CEM_STORE); do
+        local ind=''
+        if [ "$account" = "$CEM_ACCOUNT" ]; then
+          ind='='
+          [ "$account" = "$CEM_DEFAULT" ] && ind+='*' || ind+='>'
+        else
+          ind=' '
+          [ "$account" = "$CEM_DEFAULT" ] && ind+='*' || ind+=' '
+        fi
+
+        echo "${ind} ${account}"
+      done
+      echo
+      echo '# => - current'
+      echo '# =* - current & default'
+      echo '#  * - default'
+      ;;
+  esac
+}
+###############################################################################
+# TEM is a utility for managing multiple environments. It handles loading a   #
+# default set of variables, ensures the litany of variables used by the tools #
+# are set and provides a mechanism for quickly switching between profiles.    #
+############################################################################### 
+tem(){
+  APPLICATION_NAME='The Environment Manager'
+  COMMAND='tem'
+  if [ ! -z $1 ]; then
+    cmd=$1
+    shift
+  else
+    cmd='help'
+  fi
+  case $cmd in
+    "help" )
+      echo "${APPLICATION_NAME}"
+      echo
+      echo "Usage:"
+      echo "  ${COMMAND} help                      Show this message"
+      echo "  ${COMMAND} create <account>          Create a new AWS account"
+      echo "  ${COMMAND} remove <account>          Remove AWS account"
+      echo "  ${COMMAND} use <account>             Use the named AWS account"
+      echo "  ${COMMAND} do <account> <command...> Run a command under the given account"
+      echo "  ${COMMAND} default <account>         Set the default account"
+      echo "  ${COMMAND} list                      Show all available accounts"
+      ;;
+    "create" )
+      local account
+      local store
+
+      if [ $# -lt 1 ]; then
+        echo "Usage: ${COMMAND} create <account>"
+        return 1
+      fi
+
+      account=$1
+      store=${TEM_STORE}/${account}
+      shift
+
+      if [ -f $store ]; then
+        echo "Profile named ${account} already exists!"
+        return 1
+      fi
+
+      touch $store
+
+      echo "Profile ${account} created! Edit ${store}, switch with ${COMMAND} use ${account}"
+      ;;
+    "remove")
+      local account
+      local store
+
+      if [ $# -lt 1 ]; then
+        echo "Usage: ${COMMAND} remove <account>"
+        return 1
+      fi
+
+      account=$1
+      store=${TEM_STORE}/${account}
+      shift
+
+      if [ -f $store ]; then
+        rm $store
+        return 0
+      else
+        echo "Profile ${account} does not exist at ${store}"
+        return 1
+      fi
+      ;;
+    "use" )
+      local account
+      local store
+
+      if [ $# -lt 1 ]; then
+        echo "Usage: ${COMMAND} use <account>"
+        return 1
+      fi
+
+      account=$1
+      shift
+
+      if [ $account = 'default' ]; then
+        if [ -z $TEM_DEFAULT ]; then
+          echo "No default account has been configured. Set one with aam default <account>."
+          return 1
+        fi
+        account=$TEM_DEFAULT
+      fi
+
+      store=${TEM_STORE}/${account}
+      if [ ! -f $store ]; then
+        echo "No account named ${account}"
+        return 1
+      fi
+
+      old_store=${TEM_STORE}/${TEM_ACCOUNT}
+      export TEM_ACCOUNT=$account
+      for key in $(awk -F'[#=\ ]' '$1 ~ /export/ {print $2}' ${old_store}); do
+        unset $key
+      done
+      source ${store}
+      echo "Switched to account ${account}"
+      ;;
+    "do" )
+      local account
+
+      if [ $# -lt 1 ]; then
+        echo "Usage: ${COMMAND} do <account> <command...>"
+        return 1
+      fi
+
+      account=$1
+      store=${TEM_STORE}/${account}
+      shift
+      command=$*
+
+      if [ ! -f $store ]; then
+        echo "No account named ${account}"
+        return 1
+      fi
+
+      $SHELL -l -c "source ${EM_SCRIPT}; ${COMMAND} use ${account}; ${command}"
+      ;;
+    "default" )
+      local account
+
+      if [ $# -lt 1 ]; then
+        echo "Usage: ${COMMAND} default <account>"
+        return 1
+      fi
+
+      account=$1
+      store=${TEM_STORE}/${account}
+      shift
+
+      if [ ! -f $store ]; then
+        echo "No account named ${account}"
+        return 1
+      fi
+
+      echo $account > $TEM_DEFAULT_FILE
+      echo "Default account set to ${account}"
+      ;;
+    "list" )
+      echo
+      echo "Available accounts"
+      for account in $(ls $TEM_STORE); do
+        local ind=''
+        if [ "$account" = "$TEM_ACCOUNT" ]; then
+          ind='='
+          [ "$account" = "$TEM_DEFAULT" ] && ind+='*' || ind+='>'
+        else
+          ind=' '
+          [ "$account" = "$TEM_DEFAULT" ] && ind+='*' || ind+=' '
+        fi
+
+        echo "${ind} ${account}"
+      done
+      echo
+      echo '# => - current'
+      echo '# =* - current & default'
+      echo '#  * - default'
+      ;;
+  esac
+}
+if [ -z $AAM_ACCOUNT ]; then
+  aam use default > /dev/null
+fi
+
+if [ -z $CEM_ACCOUNT ]; then
+  cem use default > /dev/null
+fi
+
+if [ -z $TEM_ACCOUNT ]; then
+  tem use default > /dev/null
+fi
